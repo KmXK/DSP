5ce4feda4ff7a8edefc22d0bcf4b2690d8dcc2cc
KmX <guydo.k@yandex.by>
2023-10-20 15:03:16 +0300

    Added removing animation

--- src/app/signal-action-list/signal-action-list.component.ts
+++ src/app/signal-action-list/signal-action-list.component.ts
@@ -1,20 +1,38 @@
 import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector: 'app-signal-action-list',
     templateUrl: './signal-action-list.component.html',
-    styleUrls: ['./signal-action-list.component.scss']
+    styleUrls: ['./signal-action-list.component.scss'],
+    animations: [
+        trigger('removing', [
+            state('removed', style({
+                left: -2000,
+                color: 'red'
+            })),
+            transition('* => removed', [
+                animate(500)
+            ])
+        ])
+    ]
 })
 export class SignalActionListComponent {
     actionTypes = ['Combination', 'Modulation'];
-
     actions: string[] = ['Combination', 'Modulation'];
+    states: string[] = [];
 
     addAction(type: string) {
         this.actions = [type, ...this.actions];
+        this.states = ['', ...this.states];
     }
 
     removeItem(index: number) {
-        this.actions.splice(index, 1);
+        this.states[index] = 'removed';
+
+        setTimeout(() => {
+            this.actions.splice(index, 1);
+            this.states.splice(index, 1);
+        }, 500);
     }
 }
